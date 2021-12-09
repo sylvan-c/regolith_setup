7664ab4cd0e8650f7c8b55881d832e33f0d5fe2f
Sylvan <sylvanchouhan@gmail.com>
2021-12-09 23:27:50 +0000

    update setup script

--- regolith_setup.sh
+++ regolith_setup.sh
@@ -6,4 +6,4 @@ sudo cp -r ./conf.d /etc/regolith/i3xrocks/conf.d
 sudo mv /usr/share/i3xrocks /usr/share/i3xrocks.bkp
 sudo cp ./i3xrocks /usr/share/i3xrocks
 
-cp ./regolith /home/$USER/.config/regolith
+cp ./config /home/$USER/.config/regolith/i3/
